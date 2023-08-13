97688ca440f03bdb498f5d0110421028c8b498e2
Jari <zalmanhajari@gmail.com>
2023-08-13 21:42:11 +0800

    finished the main design

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+import logo from "./logo-cookiejari.jpg";
+
+// Add the image to our existing div.
+// const myLogo = new Image();
+// myLogo.src = logo;
+
+// element.appendChild(myLogo);
+
+// console.log("WOW");
+
+function createHeader() {}
+
+function component() {
+  const content = document.getElementById("content");
+
+  return content;
+}
+
+document.body.appendChild(component());
