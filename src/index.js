5755f6f967abeaa0dda780d56548276ac6de7fb0
Jari <zalmanhajari@gmail.com>
2023-08-20 17:17:22 +0800

    header and nav is finished

--- src/index.js
+++ src/index.js
@@ -9,10 +9,73 @@ import logo from "./logo-cookiejari.jpg";
 
 // console.log("WOW");
 
-function createHeader() {}
+function createHeader() {
+  const header = document.createElement("header");
+
+  const headerName = document.createElement("div");
+  headerName.classList.add("resto-name");
+  const restoName = document.createElement("h1");
+  restoName.textContent = "CookieJari";
+  headerName.appendChild(restoName);
+
+  header.appendChild(headerName);
+
+  header.appendChild(createNav());
+  return header;
+}
+
+function createNav() {
+  const nav = document.createElement("nav");
+
+  //home
+  const homeLi = document.createElement("li");
+  nav.appendChild(homeLi);
+  const homeDiv = document.createElement("div");
+  homeLi.appendChild(homeDiv);
+  homeDiv.classList.add("home");
+  homeLi.appendChild(homeDiv);
+  const home = document.createElement("a");
+  home.textContent = "Home";
+  home.addEventListener("click", (e) => {
+    console.log("home");
+  });
+  homeDiv.appendChild(home);
+
+  //menu
+  const menuLi = document.createElement("li");
+  nav.appendChild(menuLi);
+  const menuDiv = document.createElement("div");
+  menuLi.appendChild(menuDiv);
+  menuDiv.classList.add("menu");
+  menuLi.appendChild(menuDiv);
+  const menu = document.createElement("a");
+  menu.textContent = "Menu";
+  menu.addEventListener("click", (e) => {
+    console.log("Menu");
+  });
+  menuDiv.appendChild(menu);
+
+  //contact
+  const contactLi = document.createElement("li");
+  nav.appendChild(contactLi);
+  const contactDiv = document.createElement("div");
+  contactLi.appendChild(contactDiv);
+  contactDiv.classList.add("contact");
+  contactLi.appendChild(contactDiv);
+  const contact = document.createElement("a");
+  contact.textContent = "Contact";
+  contact.addEventListener("click", (e) => {
+    console.log("contact");
+  });
+  contactDiv.appendChild(contact);
+
+  return nav;
+}
 
 function component() {
   const content = document.getElementById("content");
+
+  content.append(createHeader());
 
   return content;
 }
